8d74501f92340d72e1117d97b6c738da1f878759
jonatasem <jonatas.em20@gmail.com>
2025-04-24 14:07:06 -0300

    feat: adicionando a rota get movies/id

--- routes/movies.js
+++ routes/movies.js
@@ -7,4 +7,15 @@ router.get('/', (req, res) => {
     res.json(movies); // Retorna a lista de filmes em formato JSON
 });
 
+// Rota para obter detalhes de um filme pelo ID
+router.get('/:id', (req, res) => {
+    const movieId = parseInt(req.params.id, 10);
+    const movie = movies.find(m => m.id === movieId);
+    if (movie) {
+        res.json(movie);
+    } else {
+        res.status(404).json({ message: 'Filme não encontrado' });
+    }
+});
+
 module.exports = router;
